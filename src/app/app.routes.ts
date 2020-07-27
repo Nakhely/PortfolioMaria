ca14842a0fd4a352104a102150108e77a0585cd3
Nakhely <mariaortega.alumno@gmail.com>
2020-07-27 20:02:18 +0200

    contact with smooth animate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import {RouterModule, Routes} from '@angular/router';
 import {AboutComponentComponent} from './about-component/about-component.component';
 import {MyworksComponentComponent} from './myworks-component/myworks-component.component';
+import {ContactComponentComponent} from './contact-component/contact-component.component';
 
 /* Fichero de rutas para el navbar */
 
 const APP_ROUTES: Routes = [
   {path: 'about_me', component: AboutComponentComponent},
   {path: 'my_works', component: MyworksComponentComponent},
+  {path: 'contact', component: ContactComponentComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'about_me'}
 ];
 
