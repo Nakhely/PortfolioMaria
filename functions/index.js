96de3901a43913c56074618ed5d19ab5cdda7ff0
Jackdaw16 <alejandro.demetrio13@gmail.com>
2020-07-29 19:01:51 +0100

    firebase function email added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require(`firebase-admin`);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+require('dotenv').config()
+
+const {SENDER_EMAIL, SENDER_PASSWORD} = process.env;
+
+exports.sendEmailNotification = functions.firestore.document('submissions/{docId}').onCreate( (snapshot, ctx) => {
+  const data = snapshot.data();
+  let authData = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: SENDER_EMAIL,
+      pass: SENDER_PASSWORD
+    }
+  });
+
+  authData.sendMail({
+    from: 'mariaom.jobs@gmail.com',
+    to: `${data.email}`,
+    subject: 'Contacto',
+    text: `${data.message}`,
+    html: `${data.email}`,
+  }).then( res => console.log('succesfully sent that mail')).catch( error => console.log(error));
+})
+
+
